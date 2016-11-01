f5434b77336869ee202263afd2fdfa3a7a0d7cfa
Antoine S <antoine.stempfer@epitech.eu>
2016-11-01 08:55:40 +0100

    Added tilemap

---
+++ Assets/Sprites/TiledMap.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TiledMap : MonoBehaviour {
+
+    public int Width;
+    public int Height;
+
+    public GameObject[] Tiles;
+
+    public int[,] map { private set; get; }
+    
+    void Start()
+    {
+        map = new int[Width, Height];
+    }
+    
+    public void SetAt(int x, int y, int value)
+    {
+        map[x, y] = value;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        for (float i = transform.position.x; i <= transform.position.x + Width; i++)
+        {
+            Gizmos.DrawLine(new Vector2(i, transform.position.y), new Vector2(i, transform.position.y + Height));
+        }
+
+        for (float j = transform.position.y; j <= transform.position.y + Height; j++)
+        {
+            Gizmos.DrawLine(new Vector2(transform.position.x, j), new Vector2(transform.position.x + Width, j));
+        }
+    }
+}
