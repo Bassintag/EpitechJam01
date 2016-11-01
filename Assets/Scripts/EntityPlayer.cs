68c034c29f3251ce328744a90ae175eaba86e89f
baris_f <florent.baris@epitech.eu>
2016-11-01 14:50:07 +0100

    lol

--- Assets/Scripts/EntityPlayer.cs
+++ Assets/Scripts/EntityPlayer.cs
@@ -3,26 +3,38 @@ using System.Collections;
 
 public class EntityPlayer : Entity   {
 
-    Vector2 last; 
-    
+    Vector2 last;
+    float timer = 0f;
+
     public override void OnAction()
     {
         Vector2 pos = (Vector2)transform.localPosition - new Vector2(0.5f, 0.5f);
-            if (map.GetAt((int)pos.x + (int)last.x, (int)pos.y + (int)last.y).Solid == false)
-                pos += last;
+        if (map.GetAt((int)pos.x + (int)last.x, (int)pos.y + (int)last.y).Solid == false)
+        {
+            if (map.GetAt((int)pos.x + (int)last.x, (int)pos.y + (int)last.y).Entity == true)
+            {
+                GameManager.instance.combo = 0;
+            }
+            pos += last;
+        }
         transform.localPosition = (Vector3)pos + new Vector3(0.5f, 0.5f, -1f);
         last = new Vector2(0, 0);
     }
 
     void Update()
     {
-        if (Input.GetAxis("Vertical") > 0.8f)
-            last = new Vector2(0, 1);
-        else if (Input.GetAxis("Vertical") < -0.8f)
-            last = new Vector2(0, -1);
-        else if (Input.GetAxis("Horizontal") > 0.8f)
-            last = new Vector2(1, 0);
-        else if (Input.GetAxis("Horizontal") < -0.8f)
-            last = new Vector2(-1, 0);
+        timer -= Time.deltaTime;
+        if (timer <= 0f)
+        {
+            timer = 0.15f;
+            if (Input.GetAxis("Vertical") > 0.8f)
+                last = new Vector2(0, 1);
+            else if (Input.GetAxis("Vertical") < -0.8f)
+                last = new Vector2(0, -1);
+            else if (Input.GetAxis("Horizontal") > 0.8f)
+                last = new Vector2(1, 0);
+            else if (Input.GetAxis("Horizontal") < -0.8f)
+                last = new Vector2(-1, 0);
+        }
     }
 }
