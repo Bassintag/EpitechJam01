09500a65658e57b6261231cdc671262f4875fe7c
baris_f <florent.baris@epitech.eu>
2016-11-01 17:31:46 +0100

    jesaispacekegéfé

---
+++ Assets/Scripts/EntityZombie.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EntityZombie : Entity{
+
+    bool move = false;
+
+    public override void OnAction()
+    {
+        if (Mathf.Abs(x) == 1)
+        {
+
+        }
+    }
+}
