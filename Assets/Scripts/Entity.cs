55108f9fa1d25d41325f760026582a6bceb363a3
baris_f <florent.baris@epitech.eu>
2016-11-01 11:22:08 +0100

    sf
    df

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public abstract class Entity : MonoBehaviour {
 
+    public TiledMap map { get; set; }
+
     public abstract void OnAction();
 
 }
---
+++ Assets/Scripts/EntityPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EntityPlayer : Entity   {
+
+    public override void OnAction()
+    {
+        Vector2 pos = (Vector2)transform.position - new Vector2(0.5f, 0.5f);
+        if (Input.GetAxis("Vertical") > 0.8f)
+        {
+            if (true)
+            {
+
+            }
+        }
+        transform.position = pos + new Vector2(0.5f, 0.5f);
+    }
+}
